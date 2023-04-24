fc13f579cdf709b197d75b2c3f06cd4b3796004d
Heidi Kinikini <Duckwitz52@gmail.com>
2023-04-23 20:52:55 -0600

    Third Commit JavascriptSF.js

--- codeSongFluent.js/javascript.js
+++ codeSongFluent.js/javascript.js
@@ -24,6 +24,76 @@ let cMajScale = [
     ]
 ]
 
+let gMajScale = [
+
+    ['G', 'Am', 'Bm', 'C', 'D', 'Em', 'F#dim'],
+
+    [
+        'G 1st Inversion', 'G 2nd Inversion', 'Am 1st Inversion', 'Am 2nd Inversion', 'Bm 1st Inversion', 'Bm 2nd Inversion', 'C 1st Inversion', 'C 2nd Inversion', 'D 1st Inversion', 'D 2nd Inversion', 'Em 1st Inversion', 'Em 2nd Inversion', 'F#dim 1st Inversion', 'F#dim 2nd Inversion'
+    ],
+
+    ['G6', 'C6', 'D6'],
+
+    [
+        'Gmaj7', 'Am7', 'Bm7', 'Cmaj7', 'D7', 'Em7'
+    ],
+    ['Gmaj9', 'Am9', 'Cmaj9', 'D9'],
+
+    ['Gsus4', 'Asus4', 'Bsus4', 'Dsus4', 'Esus4']
+
+]
+
+let fMajScale = [
+    ['F', 'Gm', 'Am', 'Bb', 'C', 'Dm', 'Edim'],
+
+    [
+        'F 1st Inversion', 'F 2nd Inversion', 'Gm 1st Inversion', 'Gm 2nd Inversion', 'Am 1st Inversion', 'Am 2nd Inversion', 'Bb 1st Inversion', 'Bb 2nd Inversion', 'C 1st Inversion', 'C 2nd Inversion', 'Dm 1st Inversion', 'Dm 2nd Inversion', 'Edim 1st Inversion', 'Edim 2nd Inversion'
+    ],
+
+    ['F6', 'Gm6', 'Bb6', 'C6'],
+
+    ['Fmaj7', 'Gm7', 'Bbmaj7', 'C7', 'Dm7'],
+
+    ['Fmaj9', 'Gm9', 'Bbmaj9', 'C9', 'Dm9'],
+
+    [
+        'Fsus4', 'Gsus4', 'Asus4', 'Csus4', 'Dsus4'
+    ], 
+
+]
+
+
+let aMajScale = [
+    ['A', 'Bm', 'C#m', 'D', 'E', 'F#m', 'G#dim'],
+
+    [
+        'A 1st Inversion', 'A 2nd Inversion', 'Bm 1st Inversion', 'Bm 2nd Inversion', 'C#m 1st Inversion', 'C#m 2nd Inversion', 'D 1st Inversion', 'D 2nd Inversion','E 1st Inversion', 'E 2nd Inversion', 'F#m 1st Inversion', 'F#m 2nd Inversion', 'G#dim 1st Inversion', 'G#dim 2nd Inversion'
+],
+
+    ['A6', 'Bm6', 'D6', 'E6'],
+
+    [
+        'Amaj7', 'Bm7', 'C#m7', 'Dmaj7', 'E7', 'F#m7'
+],
+    ['Amaj9', 'Bm9', 'Dmaj9', 'E9', 'F#m9'],
+
+    ['Asus4', 'Bsus4', 'C#sus4', 'Esus4', 'F#sus4']
+
+]
+
+let bMajScale = [
+
+]
+
+let dMajScale = [
+
+]
+
+let eMajScale = [
+
+]
+
+
 const randomizeChords = (arr) => {
 
     
@@ -41,3 +111,15 @@ const randomizeChords = (arr) => {
 }
 
 console.log(randomizeChords(cMajScale))
+
+function chooseGenre (genre){
+    if(genre === 'pop'){
+        return 'Suggested keys: C major Scale, G major Scle, or F major'
+    } else if(genre === 'jazz'){
+        return 'Suggested keys: F Major Scale, Bb Scale, Eb Scale, or Ab major Scale'
+    } else if(genre === 'funk'){
+        return 'Suggested keys: A Major Scale, E Major Scale, G Major Scale, or D Major Scale'
+    } else if (genre === 'r&b'){
+        return 'Suggested keys: '
+    }
+} console.log(chooseGenre('funk'))
